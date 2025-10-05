8ac5b842fa535d9cefca1a0655993f8b1fa74579
ndprime <nikechideradivine@gmail.com>
2025-10-05 02:36:57 +0100

    imported all photos

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./assets/images/logo.svg";
+import outline2 from "./assets/images/outline (2).png";
+import outline1 from "./assets/images/outline (1).png";
+import outline from "./assets/images/outline.png";
+import vector from "./assets/images/Vector.png";
+import mintemp from "./assets/images/Vector (2).png";
+import maxtemp from "./assets/images/Vector (1).png";
+import axios from "axios";
+
+function App() {
+  <>
+    </>
+  );
+}
+
+export default App;
